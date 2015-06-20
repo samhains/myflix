cb3f0ef2b1b8c23245b223c22b5ded9d6013b33b
Sam Hains <sdh@eml.cc>
2015-06-20 15:21:15 -0600

    added route directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'video#index'
-
+  root to: 'video#index'
   resources :video
   resources :category, only: [:show]
 
