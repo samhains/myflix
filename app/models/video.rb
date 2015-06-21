632d08f0364419d20077d85bf893dc3bace75826
Sam Hains <sdh@eml.cc>
2015-06-20 23:34:31 -0600

    added validation tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,5 @@
 class Video < ActiveRecord::Base
   has_many :category_videos
   has_many :categories, -> {order( :name )}, through: :category_videos
+  validates_presence_of :title, :description
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-
 describe Video do
   it "it can save itself" do
     video = Video.new(title: 'Speed', description: 'Action Flick')
@@ -12,5 +11,15 @@ describe Video do
     categories = Category.create([{name: 'Action'}, {name: 'Drama'}])
     Video.create(title: 'Speed', description: 'Action Flick', categories: categories)
     expect(Video.first.categories).to eq(categories)
+  end
+
+  it "must have a title" do
+    video = Video.create(description: "A description")
+    expect(Video.count).to eq(0)
+  end
+
+  it "must have a description" do
+    video = Video.create(title: "A title")
+    expect(Video.count).to eq(0)
   end
 end
