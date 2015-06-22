414d13d86612b5f30bc7a65836d740cb9bbc8c64
Sam Hains <sdh@eml.cc>
2015-06-21 18:06:24 -0600

    changed categories partial position

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,8 @@
 require 'spec_helper'
 
 describe Video do
-  it "it can save itself" do
-    video = Video.new(title: 'Speed', description: 'Action Flick')
-    video.save
-    expect(Video.first).to eq(video)
-  end
 
-  it "can have many categories" do
-    categories = Category.create([{name: 'Action'}, {name: 'Drama'}])
-    Video.create(title: 'Speed', description: 'Action Flick', categories: categories)
-    expect(Video.first.categories).to eq(categories)
-  end
-
-  it "must have a title" do
-    video = Video.create(description: "A description")
-    expect(Video.count).to eq(0)
-  end
-
-  it "must have a description" do
-    video = Video.create(title: "A title")
-    expect(Video.count).to eq(0)
-  end
+  it { should have_many(:categories)}
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:description)}
 end
